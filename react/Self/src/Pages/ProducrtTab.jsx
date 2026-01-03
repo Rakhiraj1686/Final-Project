9efcfae3e890d163776f72fe3877e2f795efb881
Rakhiraj1686 <rakhiraj1686@gmail.com>
2026-01-04 01:53:36 +0530

    self

--- react/Self/src/Pages/ProducrtTab.jsx
+++ react/Self/src/Pages/ProducrtTab.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Product from "./Product.jsx";
 
 const ProductTab = () => {
-    return (
-        <>
-            <Product title="Phone" price="30,000"/>
-            <Product title ="Laptop" price="40,000" />
-            <Product title ="Pen" price="10,000"/>
-        </>
-    )
-}
+  let Options = ["hi-tech", "durable", "fast"];
+  return (
+    <>
+      <Product title="Phone" price={30000} features={Options} />
+      <Product title="Laptop" price={40000} />
+      <Product title="Pen" price={10000} />
+    </>
+  );
+};
 export default ProductTab;
--- react/Self/src/Pages/Product.jsx
+++ react/Self/src/Pages/Product.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Product = ({ title, price }) => {
+const Product = ({ title, price, features }) => {
   return (
     <>
       <div className="flex justify-center">
-        <div className="Product h-20 w-30 border mt-10 p-3">
-          <h3>{title}</h3>
+        <div className="Product h-20 w-30 border mt-10 p-3 rounded-2xl">
+          <h3 className="font-bold">{title}</h3>
           <h5>Price : {price}</h5>
+          <p>{features}</p>
         </div>
       </div>
     </>
