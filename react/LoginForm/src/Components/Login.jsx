1b3e90f42d6f3ab8e75829987f07a3808516f6d8
Rakhiraj1686 <rakhiraj1686@gmail.com>
2026-01-05 23:43:18 +0530

    Login

--- react/LoginForm/src/Components/Login.jsx
+++ react/LoginForm/src/Components/Login.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 const Login = () => {
   return (
     <>
+    <div>
+      <h1>Login Form</h1>
+      <div>
+        <label htmlFor="username"></label>
+        <input type="text" name="username" id="username" placeholder='Enter your username' />
+      </div>
+    </div>
     </>
   )
 }
